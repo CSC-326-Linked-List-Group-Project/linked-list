9d24e52a7bd820f0d7534ab6609afeb93b1fae49
bkhzhou <79166987+bkhzhou@users.noreply.github.com>
2021-03-22 23:16:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
-// this will be the main code
-// you guys can add repositories and stuff
-// for example
+//CSC 326
+//SI EDITOR PHASE 1
+//GROUP MEMBERS: DEANNA MANDARINO, VICTORY JACZYNSKI, SIMON CHRISTIAN, BILL ZHOU
+#include "LinkedList.h"
+#include "Editor.h"
+#include <iostream>
+#include <string>
+
+void main(int argc, const char* argv[]) {
+
+	if (argc > 1) {
+		Editor si(argv[1]);
+	}
+	else
+		exit(1);
+}
