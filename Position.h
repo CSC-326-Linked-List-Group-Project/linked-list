2582811f8ad762d5d15061b369821ada4557317c
victoria0514 <69165814+victoria0514@users.noreply.github.com>
2021-03-24 12:48:36 -0400

    Update Position.h

--- Position.h
+++ Position.h
@@ -1,3 +1,13 @@
+//CSC 326
+//VI EDITOR PHASE 1
+//GROUP MEMBERS: DEANNA MANDARINO, VICTORIA JACZYNSKI, SIMON CHRISTIAN, BILL ZHOU
+
+// Task: Write an Editor class that has an instance of our LinkedList
+// Use command line arguments - the user will specify the file (if it exists)
+// Code a constructor for the Editor class with one parameter for the file name - read it in and add to the linked list 
+// Allow for the following commands:  :q   :w
+
+
 #ifndef Position_H
 #define Position_H
 
