33e1e1b740deb7028b33469d4d8f3313b80f3848
bkhzhou <79166987+bkhzhou@users.noreply.github.com>
2021-03-23 23:25:17 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 //CSC 326
 //SI EDITOR PHASE 1
 //GROUP MEMBERS: DEANNA MANDARINO, VICTORY JACZYNSKI, SIMON CHRISTIAN, BILL ZHOU
-#include "LinkedList.h"
-#include "Editor.h"
 #include <iostream>
 #include <string>
+#include "LinkedList.h"
+#include "Editor.h"
 
 void main(int argc, const char* argv[]) {
 
 	if (argc > 1) {
 		Editor si(argv[1]);
 	}
-	else
-		exit(1);
 }
