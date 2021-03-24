ac71a3260846827636cfff6fd6b7f7ec221b86ff
bkhzhou <79166987+bkhzhou@users.noreply.github.com>
2021-03-24 12:48:00 -0400

    Update editor.cpp

--- editor.cpp
+++ editor.cpp
@@ -1,13 +1,3 @@
-//CSC 326
-//VI EDITOR PHASE 1
-//GROUP MEMBERS: DEANNA MANDARINO, VICTORY JACZYNSKI, SIMON CHRISTIAN, BILL ZHOU
-
-// Task: Write an Editor class that has an instance of our LinkedList
-// Use command line arguments - the user will specify the file (if it exists)
-// Code a constructor for the Editor class with one parameter for the file name - read it in and add to the linked list 
-// Allow for the following commands:  :q   :w
-
-
 #include "Editor.h"
 #include "ListInterface.h"
 #include "Position.h"
@@ -15,7 +5,7 @@
 #include <string>
 #include <fstream>
 #include <conio.h>
-void placeCursorAt(Position coordinate); //Not a method of Editor but a package with Editor
+void placeCursorAt(Position coordinate);
 using namespace std;
 
 Editor::Editor() {
@@ -27,9 +17,12 @@ Editor::Editor(string _fileName) {
 	fileName = _fileName;
 	int lineCounter = 1;
 
+	/*Exits when file is not found*/
 	if (inFile.fail()) {
 		cout << "File cannot be found.";
+		exit(1);
 	}
+	/*Reads each line of the file*/
 	while (!inFile.eof()) {
 		getline(inFile, readLine);
 		lines.insert(lineCounter, readLine);
@@ -38,6 +31,8 @@ Editor::Editor(string _fileName) {
 }
 
 void Editor::displayLines() {
+	/*Clear screen to display text file*/
+	system("CLS");
 	for (int i = 1; i < lines.getLength(); i++) {
 		cout << lines.getEntry(i) << endl;
 	}
@@ -59,11 +54,11 @@ void Editor::run() {
 		placeCursorAt(cursor);
 		cout << entry;
 		entry = _getch();
-		
-		entry = _getche();
+		/*q quits the Editor and closes the file*/
 		if (entry == 'q') {
+			entry = _getche();
 			inFile.close();
-			cout << "\nsuccess" << endl;
+			cout << "\nClosing the file was successful." << endl;
 		}
 		else if (entry == 'w') {
 			/*Does something*/
@@ -71,7 +66,8 @@ void Editor::run() {
 	}
 
 }
-
+/*This is not a member function but a package for Editor*/
+/*Initializes the cursor*/
 void placeCursorAt(Position coordinate) {
 	COORD coord;
 	coord.X = coordinate.getX();
