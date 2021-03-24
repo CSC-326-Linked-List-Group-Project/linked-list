a206fd37bf8c39b0b9bfeb4baa77a0631714d2a4
victoria0514 <69165814+victoria0514@users.noreply.github.com>
2021-03-24 12:45:38 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 //CSC 326
-//SI EDITOR PHASE 1
+//VI EDITOR PHASE 1
 //GROUP MEMBERS: DEANNA MANDARINO, VICTORY JACZYNSKI, SIMON CHRISTIAN, BILL ZHOU
+
+// Task: Write an Editor class that has an instance of our LinkedList
+// Use command line arguments - the user will specify the file (if it exists)
+// Code a constructor for the Editor class with one parameter for the file name - read it in and add to the linked list 
+// Allow for the following commands:  :q   :w
+
 #include <iostream>
 #include <string>
 #include "LinkedList.h"
