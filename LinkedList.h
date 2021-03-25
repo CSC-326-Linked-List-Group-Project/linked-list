8da93f26cbac7bd4f7ff39993dfefbffc0bd723c
bkhzhou <79166987+bkhzhou@users.noreply.github.com>
2021-03-24 20:29:03 -0400

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,53 +1,61 @@
 //Â  Created by Frank M. Carrano and Timothy M. Henry.
 //Â  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
 
-/** ADT list: Link-based implementation.
-    Listing 9-2.
-    @file LinkedList.h */
+/** Interface for the ADT list
+    Listing 8-1
+    @file ListInterface.h */
 
-#ifndef LINKED_LIST_
-#define LINKED_LIST_
-
-#include "ListInterface.h"
-#include "Node.h"
-#include "PrecondViolatedExcep.h"
+#ifndef _LIST_INTERFACE
+#define _LIST_INTERFACE
 
 template<class ItemType>
-class LinkedList : public ListInterface<ItemType>
+class ListInterface
 {
-private:
-    Node<ItemType>* headPtr; // Pointer to first node in the chain;
-                             // (contains the first entry in the list)
-    int itemCount;           // Current count of list items 
-
-    // Locates a specified node in this linked list.
-    // @pre  position is the number of the desired node;
-    //       position >= 1 and position <= itemCount.
-    // @post  The node is found and a pointer to it is returned.
-    // @param position  The number of the node to locate.
-    // @return  A pointer to the node at the given position.
-    Node<ItemType>* getNodeAt(int position) const;
-
 public:
-    LinkedList();
-    LinkedList(const LinkedList<ItemType>& aList);
-    virtual ~LinkedList();
+    /** Sees whether this list is empty.
+     @return True if the list is empty; otherwise returns false. */
+    virtual bool isEmpty() const = 0;
 
-    bool isEmpty() const;
-    int getLength() const;
-    bool insert(int newPosition, const ItemType& newEntry);
-    bool remove(int position);
-    void clear();
+    /** Gets the current number of entries in this list.
+     @return The integer number of entries currently in the list. */
+    virtual int getLength() const = 0;
 
-    /** @throw PrecondViolatedExcep if position < 1 or
-                                       position > getLength(). */
-    ItemType getEntry(int position) const throw(PrecondViolatedExcep);
+    /** Inserts an entry into this list at a given position.
+     @pre  None.
+     @post  If 1 <= position <= getLength() + 1 and the insertion is
+        successful, newEntry is at the given position in the list,
+        other entries are renumbered accordingly, and the returned
+        value is true.
+     @param newPosition  The list position at which to insert newEntry.
+     @param newEntry  The entry to insert into the list.
+     @return  True if insertion is successful, or false if not. */
+    virtual bool insert(int newPosition, const ItemType& newEntry) = 0;
 
-    /** @throw PrecondViolatedExcep if position < 1 or
-                                       position > getLength(). */
-    void replace(int position, const ItemType& newEntry)
-        throw(PrecondViolatedExcep);
-}; // end LinkedList
+    /** Removes the entry at a given position from this list.
+     @pre  None.
+     @post  If 1 <= position <= getLength() and the removal is successful,
+        the entry at the given position in the list is removed, other
+        items are renumbered accordingly, and the returned value is true.
+     @param position  The list position of the entry to remove.
+     @return  True if removal is successful, or false if not. */
+    virtual bool remove(int position) = 0;
 
-#include "LinkedList.cpp"
-#endif 
+    /** Removes all entries from this list.
+     @post  List contains no entries and the count of items is 0. */
+    virtual void clear() = 0;
+
+    /** Gets the entry at the given position in this list.
+     @pre  1 <= position <= getLength().
+     @post  The desired entry has been returned.
+     @param position  The list position of the desired entry.
+     @return  The entry at the given position. */
+    virtual ItemType getEntry(int position) const = 0;
+
+    /** Replaces the entry at the given position in this list.
+     @pre  1 <= position <= getLength().
+     @post  The entry at the given position is newEntry.
+     @param position  The list position of the entry to replace.
+     @param newEntry  The replacement entry. */
+    virtual void replace(int position, const ItemType& newEntry) = 0;
+}; // end ListInterface
+#endif
